c1d3d09b54ec5f743a1ba62945f3b17a7f1ea3d9
stanleycai95 <46463107+stanleycai95@users.noreply.github.com>
2023-10-23 12:03:36 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -70,4 +70,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=10000, log_level="info")
